964ca498a16ad117fe8b32858a6b553af540e209
chezhian <chezhian.claritaz@gmail.com>
2021-10-07 19:53:48 +0530

    + task completed

--- image_classifier/settings.py
+++ image_classifier/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'image_classifier.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database.sqlite3',
     }
 }
 
